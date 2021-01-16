acdd3af10f6dd6da8bab9082a10c6f992bffe472
R04DM4N <58348842+Tofunmiakin@users.noreply.github.com>
2021-01-16 17:20:52 +0100

    sign in works now, linked with the front end

--- server.js
+++ server.js
@@ -1,6 +1,70 @@
 const express = require ('express');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const knex = require('knex');
+
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host : '127.0.0.1',
+//     user : 'postgres',
+//     password : 'p4ssw0rd',
+//     database : 'robofriends'
+//   }
+// });
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const database ={
+  users : [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      joined: new Date()
+    },
+
+    {
+      id: '124',
+      name: 'Hue',
+      email: 'hue@gmail.com',
+      password: 'eggs',
+      joined: new Date()
+    },
+
+    {
+      id: '125',
+      name: 'Bog',
+      email: 'bog@gmail.com',
+      password: 'plants',
+      joined: new Date()
+    }
+  ]
+}
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password){
+      res.json('success');
+  } else {
+    res.status(400).json('error logging in');
+  }
+})
+
+app.post('/register', (req, res) => {
+  const {email, name, password} =req.body;
+  database.users.push({
+    id
+  })
+})
 
 app.listen(5000, () => {
   console.log('app is running on port 5000');
